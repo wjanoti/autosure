63191db84fb33e1792d2c4a4ef5254473938550a
William Janoti <wjanoti@gmail.com>
2019-02-28 13:05:12 +0100

    Small changes

--- app/src/main/java/no/uio/ifi/autosure/MainActivity.java
+++ app/src/main/java/no/uio/ifi/autosure/MainActivity.java
@@ -1,6 +1,5 @@
 package no.uio.ifi.autosure;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, ClaimsHistoryFragment.OnFragmentInteractionListener {
@@ -25,24 +22,28 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         sessionManager = new SessionManager(this);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // default fragment
+        loadFragment(ClaimsHistoryFragment.class);
+        navigationView.getMenu().getItem(1).setChecked(true);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -52,45 +53,22 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        Fragment fragment = null;
-        Class fragmentClass = null;
-
-        // keeps selected option highlighted
+        // keeps selected option highlighted on the drawer menu
         item.setChecked(true);
+
         switch (item.getItemId()) {
             case R.id.nav_profile:
                 break;
             case R.id.nav_history:
-                fragmentClass = ClaimsHistoryFragment.class;
+                loadFragment(ClaimsHistoryFragment.class);
                 break;
             case R.id.nav_new_claim:
                 break;
             case R.id.nav_logout:
-                TaskListener logoutCallback = new TaskListener<Boolean>() {
-                    @Override
-                    public void onFinished(Boolean logoutSuccessful) {
-                        if (logoutSuccessful) {
-                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                            sessionManager.clearSession();
-                            startActivity(intent);
-                        }
-                    }
-                };
-                new LogoutTask(logoutCallback, sessionManager.getSessionId()).execute();
+                logout();
         }
 
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-        setTitle(item.getTitle());
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
@@ -99,5 +77,37 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    private void logout() {
+        TaskListener logoutCallback = new TaskListener<Boolean>() {
+            @Override
+            public void onFinished(Boolean logoutSuccessful) {
+                if (logoutSuccessful) {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    sessionManager.clearSession();
+                    startActivity(intent);
+                }
+            }
+        };
+        new LogoutTask(logoutCallback, sessionManager.getSessionId()).execute();
+    }
+
+    /**
+     * Loads a fragment into the main activity.
+     *
+     * @param fragmentClass name of the fragment's class
+     */
+    private void loadFragment(Class fragmentClass) {
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.clContent, fragment).commit();
     }
 }
