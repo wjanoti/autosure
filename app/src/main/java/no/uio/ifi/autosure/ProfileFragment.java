e05f208341e54716bd7945810372fd93aeb3a05d
William Janoti <w.janoti@sportradar.com>
2019-03-13 20:58:48 +0100

    Removed unused code from profile fragment

--- app/src/main/java/no/uio/ifi/autosure/ProfileFragment.java
+++ app/src/main/java/no/uio/ifi/autosure/ProfileFragment.java
@@ -1,12 +1,9 @@
 package no.uio.ifi.autosure;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +11,9 @@ import android.widget.TextView;
 
 import no.uio.ifi.autosure.models.Customer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ProfileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
     private static final String CUSTOMER = "customer";
-    private OnFragmentInteractionListener mListener;
     private Customer customer;
 
     public ProfileFragment() {
@@ -47,7 +35,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
@@ -65,34 +52,6 @@ public class ProfileFragment extends Fragment {
         txtdateOfBirth.setText(customer.getDateOfBirth());
         txtinsurancePolicyNumber.setText(Integer.toString(customer.getPolicyNumber()));
         txtfiscalNumberTxt.setText(Integer.toString(customer.getFiscalNumber()));
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
     }
 
 }
